60f828af59807d79bf6a0fffd55ef9a11091271f
SamBaksht <sam@sambaksht.com>
2026-02-23 19:57:00 -0600

    XBOX Controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,16 +6,15 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.TeleopDrive;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.drivechain.DriveTrain;
 import frc.robot.subsystems.drivechain.SwerveDrive;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.math.MathUtil;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -27,10 +26,9 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final SwerveDrive m_swerveDrive = new SwerveDrive();
-
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandJoystick m_driverController =
-      new CommandJoystick(OperatorConstants.kDriverControllerPort);
+  private final String controllerType = "XBOX"; // XBOX or JOYSTICK
+  private CommandJoystick m_driverController;
+  private CommandXboxController m_xboxController;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -50,12 +48,21 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-    
-    m_swerveDrive.setDefaultCommand(
-        new TeleopDrive(
+    if(controllerType.equals("XBOX")) {
+      m_xboxController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+      m_swerveDrive.setDefaultCommand(
+          new TeleopDrive(
+              m_swerveDrive,
+              () -> -m_xboxController.getLeftY(),  // forward/back
+              () -> -m_xboxController.getLeftX(),  // strafe
+              () -> -m_xboxController.getRightX() // rotate
+          ));
+      m_xboxController.start().onTrue(new InstantCommand(() -> m_swerveDrive.zeroHeading()));
+
+    } else {
+      m_driverController = new CommandJoystick(OperatorConstants.kDriverControllerPort);
+      m_swerveDrive.setDefaultCommand(
+      new TeleopDrive(
             m_swerveDrive,
             // Axis 1 is usually Y (Forward/Back)
             () -> -m_driverController.getRawAxis(1), 
@@ -64,10 +71,11 @@ public class RobotContainer {
             // Axis 2 is often the Twist/Z-axis on flight sticks, 
             // or Axis 4 on some gamepads. Adjust as needed!
             () -> -m_driverController.getRawAxis(2) 
-        )
-    );
+        ));
+        m_driverController.trigger().onTrue(new InstantCommand(() -> m_swerveDrive.zeroHeading()));
+    }
 
-    m_driverController.trigger().onTrue(new InstantCommand(() -> m_swerveDrive.zeroHeading()));
+
     
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
