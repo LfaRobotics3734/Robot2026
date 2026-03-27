eba4a5e61482f33aa83ad3a02947a9a001994e7b
SamBaksht <sam@sambaksht.com>
2026-03-27 00:44:42 -0500

    PID update
    Last ditch effort for greater auto consistency

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,8 +22,8 @@ public final class Constants {
     }
 
     public static final class AutoConstants {
-        public static final double PATH_TRANSLATION_KP = 0.9;
-        public static final double PATH_ROTATION_KP = 0.9;
+        public static final double PATH_TRANSLATION_KP = 1.8;
+        public static final double PATH_ROTATION_KP = 1.8;
         public static final double MAX_AUTO_MODULE_SPEED_METERS_PER_SECOND = 2.4;
     }
 
